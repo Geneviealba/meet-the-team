019659aaa5c88e0aa3378e4a35f11efa568c4b0b
Genevie Giko <gikogenevie@yahoo.com>
2022-02-13 09:26:03 -0500

    updates with files

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@
 
 const inquirer = require("inquirer");
 
-const Employee = require("../lib/Employee");
-const Engineer = require("../lib/Engineer");
-const Intern = require("../lib/Intern");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
 const fs = require("fs");
