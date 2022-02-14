d0be805a2839c6b8708c3bed65e8e9c189eeaddd
Genevie Giko <gikogenevie@yahoo.com>
2022-02-13 22:16:03 -0500

    Codes update

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@
 
 const inquirer = require("inquirer");
 
-const Employee = require("../lib/Employee");
-const Engineer = require("../lib/Engineer");
-const Intern = require("../lib/Intern");
-const Manager = require("../lib/Manager");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
 const fs = require("fs");
 
